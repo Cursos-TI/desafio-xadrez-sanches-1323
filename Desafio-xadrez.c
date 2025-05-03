509192caa3f440810551723aa90c8033dbcfe8ae
sanches-1323 <129427555+sanches-1323@users.noreply.github.com>
2025-05-03 20:33:30 +0000

    desafio xadrez

--- Desafio-xadrez.c
+++ Desafio-xadrez.c
@@ -2,41 +2,54 @@
 
 int main(){
 
-    int bispo , rainha , torre ;
+    int bispo = 1 ;
+    int  rainha , torre;
    
 
 
         printf("--Torre--\n");
         while(torre <= 4) //codigo para o movimento da torre 
             {
-                printf("esquerda\n");
+                printf("direita\n");
                 torre++;
             }
         printf("A Torre moveu-se 5 casa para direita..\n");
-        printf("finalizando movimento da torre....\n");
+        printf("finalizando movimento da torre....\n");  
+            printf("\n");
+
+
+
 
         printf("--Bispo--\n");
         do              //codigo para o movimento da bispo 
         {   
-            if (bispo % 2 != 0 )
+            if (bispo % 2 != 0 );
             {
                printf("Cima\n");
                printf("Direita\n");
             }
             
             bispo++;
-        } while (bispo <= 10);
+        } while (bispo <= 5);
         printf("A Bispo moveu-se 5 casa na diagoal..\n");
         printf("finalizando movimento do Bispo ....\n");
-          
-        printf("--Rainha--\n");
-        for (rainha = 0 ; rainha <= 7 ; rainha++)
-        {
-            printf("esquerda\n");
-        }       
-        printf("A Rainha moveu-se 8 casa para esquerda..\n");
-        printf("finalizando movimento da rainha....\n");
-        
+            printf("\n");
+            
 
+
+
+            printf("--Rainha--\n");
+            for(rainha = 0;rainha <= 7;rainha++)
+            {
+                printf("esquerda\n");
+            }       
+            printf("A Rainha moveu-se 8 casa para esquerda..\n");
+            printf("finalizando movimento da rainha....\n");
+           
+    
+
+
+
+        
     return 0;
 }   

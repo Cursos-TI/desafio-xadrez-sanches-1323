e8085ace83c0e7ab6cc6e81a3070d332869ae52e
sanches-1323 <129427555+sanches-1323@users.noreply.github.com>
2025-06-05 00:29:42 +0000

    finalizando Desafio

--- Desafio-xadrez.c
+++ Desafio-xadrez.c
@@ -1,68 +1,85 @@
 #include <stdio.h>
 
-int main(){
+void movertorre(int n){  //movimento da torre
+    if (n > 0)
+    {
+        printf("direita\n"); 
+        movertorre( n - 1);
+    }
+}
+ 
+void moverbispo(int n){   //movimento do bispo
+    if (n > 0)
+    {
+       printf("cima\n");
+    }
+    
+}
 
-    int bispo = 1 ;
-    int  rainha , torre, cavalo;
+void moverrainha(int n){   //movimento da rainha
+    if (n > 0)
+    {
+        printf("esquerda\n"); 
+        moverrainha( n - 1);
+    }
+}
+
+int main()
+{
+
+    int bispo,cavalo,rainha = 8 ;
+    
+
+    printf("--Torre--\n");
+
+        movertorre(5);
    
-
-
-        printf("--Torre--\n");
-        while(torre <= 4) //codigo para o movimento da torre 
-            {
-                printf("direita\n");
-                torre++;
-            }
-        printf("A Torre moveu-se 5 casa para direita..\n");
-        printf("finalizando movimento da torre....\n");  
-            printf("\n");
+    printf("A Torre moveu-se 5 casa para direita..\n");
+    printf("finalizando movimento da torre....\n");
+    printf("\n");
 
 
 
-
-        printf("--Bispo--\n");
-        do              //codigo para o movimento da bispo 
-        {   
-            if (bispo % 2 != 0 );
-            {
-               printf("Cima\n");
-               printf("Direita\n");
-            }
-            
-            bispo++;
-        } while (bispo <= 5);
-        printf("O Bispo moveu-se 5 casa na diagoal..\n");
-        printf("finalizando movimento do Bispo ....\n");
-            printf("\n");
-            
-
-
-
-        printf("--Rainha--\n");
-        for(rainha = 0;rainha <= 7;rainha++)
+    printf("--Bispo--\n");
+    
+         for (bispo = 5; bispo > 0; bispo--)
         {
-            printf("esquerda\n");
-        }       
-        printf("A Rainha moveu-se 8 casa para esquerda..\n");
-        printf("finalizando movimento da rainha....\n");
-        printf("\n");
+            moverbispo(5);          //recursividade-loop 
+            printf("direita\n");        
     
+        }     
 
-        printf("--cavalo--\n");
-        while (cavalo <= 1)
-        { 
+    printf("O Bispo moveu-se 5 casa na diagoal..\n");
+    printf("finalizando movimento do Bispo ....\n");
+    printf("\n");
 
-            for (cavalo = 1; cavalo <= 2; cavalo++)
-            {
-                printf("cima\n");
-            }
-                printf("esquerda\n");  
-          cavalo++;  
-        }
-        printf("O cavalo moveu-se duas casa para cima e uma casa para esquerda...\n");
-        printf("finalizando movimento do cavalo....\n");
-    
 
+
+    printf("--Rainha--\n");
         
+        moverrainha(rainha);
+
+    printf("A Rainha moveu-se 8 casa para esquerda..\n");
+    printf("finalizando movimento da rainha....\n");
+    printf("\n");
+
+
+
+    printf("--cavalo--\n");
+      
+        for(int m1 = 0, m2 = 2;
+             m1 < m2;           //loop com multipla variavel
+             m2--){
+
+            printf("cima\n");
+                 
+    } 
+            printf("direita\n");
+
+    printf("O cavalo moveu-se 2 casa para cima e 1 casa para direita...\n");
+    printf("finalizando movimento do cavalo....\n");
+
+
+
     return 0;
-}   
+}
